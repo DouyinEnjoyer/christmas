f1c80822fd41e45a6d966b7eb1092260edf9c958
tr4ckerr <dobrossy.levente_2022b@bolyai-szakkozep.hu>
2025-12-12 10:01:24 +0100

    3-8

--- christmas/common.js
+++ christmas/common.js
@@ -1,47 +1,88 @@
 /**
  * @typedef {{what: string, who1: string, who2?: string}} PartialElf
  */
-
+ 
 /**
  *  Lekérjük a tableselectort, és regisztrálunk egy change eseménykezelőt!
  */
-
-
-
+ 
+const tableselector= document.querySelector("#tableselector")
+tableselector.addEventListener('change', Lathato)
+ 
+function Lathato(e){
+ 
+    /**
+     * @type {HTMLInputElement}
+     */
+    const radio = e.target
+ 
+    const hmtlsection = document.getElementById("htmlsection")
+    const jssection = document.getElementById("jssection")
+ 
+    if(radio.checked == true){
+ 
+        const value = radio.value
+ 
+        if(value == "htmlsection"){
+            hmtlsection.classList.remove('hide')
+            jssection.classList.add('hide')
+        }
+        else{
+            jssection.classList.remove('hide')
+            hmtlsection.classList.add('hide')
+        }
+ 
+    }
+}
+ 
+ 
 /**
  * Ez a függvény a javascript legvégén fut le, amikor már minden elem betöltött.
  * Első lépésben vizsgáljuk a checkbox értékét, és az alapján beállítjuk a többi elem
  * státuszát (ha nincs bepipálva akkor a mano2 és a muszak2 értéke nem engedélyezett)
  * Aztán feliratkozunk a change eseményre, hogy amikor változik a checkbox értéke,
  * akkor is frissüljenek a státuszok.
- * 
+ *
  * @param {HTMLInputElement} checkboxElem ami a formon belül helyezkedik el
  * @returns {void}
  */
 function initCheckbox(checkboxElem){
-
+    changeCheckboxValue(checkboxElem);
+    checkboxElem.addEventListener("change", e => {
+        changeCheckboxValue(e.target);
+    });
 }
 
+ 
 /**
- * 
+ *
  * A bemeneti checkbox értéke alapján állítja a formon belüli mano2 és muszak2 disabled
  * értékét. Ha nincs bepipálva a checkbox, akkor a disabled érték igaz lesz, tehát nem
  * módosíthatjuk őket.
  * Ha be van pipálva, akkor a disabled értéke false lesz, tehát a mezők módosíthatóak
  * a checkboxtól a formot a parentElementjének (div) a parentElementjén keresztül érjük
  * el, és a két beviteli mező azonosítója mano2 és muszak2
- * 
+ *
  * @param {HTMLInputElement} checkbox egy jelölőnégyzet
  * @returns {void}
  */
 function changeCheckboxValue(checkbox){
+    const form = checkbox.parentElement.parentElement; 
+    const mano2 = form.querySelector("#mano2");
+    const muszak2 = form.querySelector("#muszak2");
 
+    const enable = checkbox.checked === true;
+
+    mano2.disabled = !enable;
+    muszak2.disabled = !enable;
 }
 
+
+ 
 /**
- * Segédfüggvény, aminek a segítségével elkérjük a htmlformon belüli 
+ * Segédfüggvény, aminek a segítségével elkérjük a htmlformon belüli
  * manochooser azonosítójú elemet, ami tartalmazza az összes rendszerben létező manót
- * 
+ *
  * @returns {HTMLSelectElement}
  */
 function getSelectElement() {
@@ -49,9 +90,9 @@ function getSelectElement() {
     const select = htmlForm.querySelector('#manochooser');
     return select;
 }
-
+ 
 /**
- * 
+ *
  * A tömb alapján felépíti a dropdownlist opcióit.
  * Első lépésben töröljük az optionlist tartalmát, majd
  * létrehozunk egy opciót, aminek nincs value értéke,
@@ -60,31 +101,45 @@ function getSelectElement() {
  * optionlisthez. Amennyiben a who2 is definiálva van,
  * azt is hozzáfűzzük.
  * a függvény korán fut le, hiszen a dropdownlist a html-en
- * található 
- * 
+ * található
+ *
  * @param {PartialElf[]} arr az adattömb, ami alapján felépítjük az opciókat
  * @returns {void}
  */
 function initSelect(arr) {
     const select = getSelectElement();
     select.innerHTML = '';
-    createoption(select, "Válassz Manót!"); // ez a függvény még nincs implementálva, görgess lejjebb
+
+    createoption(select, "Válassz Manót!");
+
+    arr.forEach(elem => {
+        createoption(select, elem.who1, elem.who1);
+
+        if (elem.who2 !== undefined) {
+            createoption(select, elem.who2, elem.who2);
+        }
+    });
 }
 
+ 
 /**
  * Létrehoz és hozzáfűz egy új optiont a selecthez
- * 
+ *
  * @param {HTMLSelectElement} selectElement a select element
  * @param {string} label az option tag közötti szöveg
  * @param {string} [value=""] az option value értéke, alapértelmezett értéke üres string
  * @returns {void}
  */
 function createoption(selectElement, label, value = "") {
-
+    const option = document.createElement("option");
+    option.value = value;       
+    option.innerText = label;  
+    selectElement.appendChild(option);
 }
 
+ 
 /**
- * 
+ *
  * Ez a függvény azután fut le az eseménykezelőben,
  * miután a validáció sikeres volt, és összeállítottuk az objektumot.
  * Hasonlóan az inithez, az objektum who1 és a who2 (ha van) tulajdonság
@@ -96,7 +151,7 @@ function createoption(selectElement, label, value = "") {
  * mert change esemény nem keletkezik a form resetelésekor.
  * Az objektum abban az esetben, ha a "Két manót veszek fel" jelölő négyzet nincs bepipálva,
  * csak az első manó adatait tartalmazza, a másik manóhoz tartozó tulajdonságok nem definiáltak
- * 
+ *
  * @param {PartialElf} obj ez az összerakott elem
  * @param {HTMLFormElement} form az ürlap
  * @param {PartialElf[]} array az adattömb
@@ -104,24 +159,33 @@ function createoption(selectElement, label, value = "") {
  */
 function createNewElement(obj, form, array) {
 
-    // ez egy ismerős rész, ehhez nem kell nyúlni
-    array.push(obj);
-    renderTbody(array);
-    form.reset();
-    // ismerős rész vége
+    const select = getSelectElement();
+    createoption(select, obj.who1, obj.who1);
 
+    if (obj.who2 !== undefined) {
+        createoption(select, obj.who2, obj.who2);
+    }
+
+    array.push(obj);
+
+    renderTbody(array);
+
+    form.reset();
+
+    const checkbox = form.querySelector("#masodikmano");
+    changeCheckboxValue(checkbox);
 }
 
 /**
- * 
+ *
  * Mivel a műszakválasztó 1,2 vagy 3 elemet vesz fel,
- * ezért ezt át kell alakítani olyan értékké, amit a 
+ * ezért ezt át kell alakítani olyan értékké, amit a
  * felhasználónak meg szeretnénk jeleníteni. Ezt a függvényt
  * akkor hívjuk, amikor az objektumot összeállítjuk, mielőtt a
  * tömbbe beleraknánk.
  * Ha 1 az értéke akkor "Délelöttös", ha 2 akkor "Délutános", míg
  * 3 esetén az "Éjszakai" értékkel kell visszatérjen a függvény
- * 
+ *
  * @param {string} muszakValue az érték, amit a select optionjéből kapunk
  * @returns {string}
  */
